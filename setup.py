1efaacfac75855e9d42d695b87a50dc16abbbf74
kaklise <kaklise@sandia.gov>
2026-03-23 16:07:18 -0700

    install stormwater dependencies

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ EXTRAS = {
     'optional': ['plotly<6.0', 'folium', 'utm', 'openpyxl', 'geopandas', 'rasterio', 'rtree'],
     'doc': ['sphinx', 'sphinx_design', 'sphinx_rtd_theme', 'pydata_sphinx_theme', 'sphinxcontrib-bibtex'],
     'test': ['pytest', 'nbformat', 'nbconvert', 'ipykernel', 'coverage'],
+    'stormwater': ['epaswmm', 'swmmio']
 }
 
 # use README file as the long description
